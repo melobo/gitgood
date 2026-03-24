c0f7a6dcaee74bce01d0700016d30ec005887c8f
Meryl Lobo <z5591728@ad.unsw.edu.au>
2026-03-24 22:31:48 +1100

    fix: added --forceExit for the testing to finish properly

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -2,13 +2,8 @@ jest.spyOn(console, 'log').mockImplementation(() => {});
 
 import dotenv from 'dotenv';
 import path from 'path';
-import { server } from '../server';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env.test'),
   quiet: true,
-});
-
-afterAll(async () => {
-  await new Promise<void>(resolve => server.close(() => resolve()));
 });
