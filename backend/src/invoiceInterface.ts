39f837a56b42d4680552ad82981d45792b0dd888
Rutva Molkar <rutva.molkar18@gmail.com>
2026-04-21 21:53:29 +1000

    added userId as a param for invoice creation functions

--- backend/src/invoiceInterface.ts
+++ backend/src/invoiceInterface.ts
@@ -35,6 +35,7 @@ export interface PaymentDetails {
 
 export interface Invoice {
   invoiceId: string;
+  userId: string;
   status: InvoiceStatus;
   buyerName: string;
   buyerAbn: string;
@@ -61,6 +62,7 @@ export interface StatusHistoryEntry {
 }
 
 export interface CreateInvoiceInput {
+  userId: string;
   buyerName: string;
   buyerAbn: string;
   supplierName: string;
